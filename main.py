38344a7a4987ab52b352fcd728fcfb80238e635c
lejewett <logan.e.jewett@gmail.com>
2015-07-13 16:57:24 -0700

    Trouble shooting problems with windows path handling.

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def main():
 	
 	elif args.per[0] < 0:
 		out = {}
+		print "Reading infile..."
 		try:
 			with open(args.infile, "r") as infile:
 				for line in infile.readlines():
@@ -72,12 +73,13 @@ def main():
 		except Exception as inst:
 			sys.stderr.write(Fore.RED + "%s, %s, %s\n" % (sys.exc_info()[0], inst, inst.args) + Fore.RESET)
 			traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+		print "Making directories..."
 		try:
 			if not os.path.exists("tmp/output"):
 				os.makedirs("tmp/output")
 		except Exception as inst:
 			sys.stderr.write("Cannot create 'tmp' directory. Please try again.\n")
-
+		print "Making temporary files..."
 		for key, val in dict.iteritems(out):
 			try:
 				with open('tmp/%s.csv' % key, 'w') as o_file:
@@ -85,7 +87,7 @@ def main():
 			except Exception as inst:
 				sys.stderr.write(Fore.RED + "%s, %s, %s\n" % (sys.exc_info()[0], inst, inst.args) + Fore.RESET)
 				traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
-
+		print "Reading temporary files..."
 		files = glob("tmp/*.csv")
 		mapping = 	{
 					"link8": 0, "link55": 1, 
@@ -95,11 +97,11 @@ def main():
 					}
 
 		processes = []
-
+		print "Formulating temp file names..."
 		for f in files:
 			name = re.split("/|\.|\\\\", f)[1]
 			persp = mapping.get(name)
-
+			print "Spawning process..."
 			if persp != None:
 				try:
 					processes.append(subprocess.Popen(['python main.py -p %d %s %s' % (persp, f, 'tmp/output/%s.csv' % name)], shell=True))
@@ -108,6 +110,7 @@ def main():
 					traceback.print_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
 			else:
 				sys.stderr.write("Unsupported Link Type: %s.\nIf this is in Error, please check link type name.\n" % name)
+		print "Attempting conglomeration..."
 		if args.conglomerate:
 			try:
 				while(list(Set([x.poll() for x in processes])) != [0]):
